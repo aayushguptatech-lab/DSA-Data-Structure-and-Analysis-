420cada64eefe310558da74cd11519746c2f67ec
Aayush Gupta <aayush.gupta.tech@gmail.com>
2026-09-15 01:47:01 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (16.43%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -9,21 +9,40 @@
  *     }
  * }
  */
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+//         ListNode temp = head;
+
+//         HashSet<ListNode> hs = new HashSet<>();
+        
+//         while(temp != null){
+//             int val = temp.val;
+//             if(hs.contains(temp)){
+//                 return true;
+//             }
+//             hs.add(temp);
+//             temp = temp.next;
+//         }
+
+//         return false;
+        
+//     }
+// }
+
+// this is the flouid warshall method
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        HashSet<ListNode> hs = new HashSet<>();
-        
-        while(temp != null){
-            int val = temp.val;
-            if(hs.contains(temp)){
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
                 return true;
             }
-            hs.add(temp);
-            temp = temp.next;
         }
-
         return false;
         
     }
