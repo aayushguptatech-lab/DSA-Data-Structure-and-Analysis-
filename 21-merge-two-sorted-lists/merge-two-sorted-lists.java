c57d4e6909447824d81c827d508fc89f8397f029
Aayush Gupta <aayush.gupta.tech@gmail.com>
2026-09-24 18:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (56.75%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,112 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = list1;
+        int size1 = 0;
+
+        while(temp != null) {
+            size1 += 1;
+            temp = temp.next;
+        }
+
+        temp = list2;
+        int size2 = 0;
+
+        while(temp != null) {
+            size2 += 1;
+            temp = temp.next;
+        }
+
+        if(size1 == 0) {
+            return list2;
+        }
+
+        if(size2 == 0) {
+            return list1;
+        }
+
+        int n = size1 + size2;
+        int[] arr = new int[n];
+
+        int i = 0;
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        while(i < n) {
+
+            // temp1 is finished
+            if(temp1 == null) {
+                arr[i] = temp2.val;
+                temp2 = temp2.next;
+                i++;
+            }
+
+            // temp2 is finished
+            else if(temp2 == null) {
+                arr[i] = temp1.val;
+                temp1 = temp1.next;
+                i++;
+            }
+
+            else if(temp1.val == temp2.val) {
+                arr[i] = temp1.val;
+                i++;
+
+                arr[i] = temp2.val;
+                i++;
+
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+            }
+
+            else if(temp1.val < temp2.val) {
+                arr[i] = temp1.val;
+                temp1 = temp1.next;
+                i++;
+            }
+
+            else {
+                arr[i] = temp2.val;
+                temp2 = temp2.next;
+                i++;
+            }
+        }
+
+        temp1 = list1;
+        ListNode result = temp1;
+
+        int j = 0;
+
+        while(j < n) {
+
+            if(temp1 == null) {
+                // We need to add remaining nodes
+                break;
+            }
+
+            temp1.val = arr[j];
+            temp1 = temp1.next;
+            j++;
+        }
+
+        // If list1 doesn't have enough nodes,
+        // attach nodes from list2
+        if(j < n) {
+            temp1 = list1;
+
+            while(temp1.next != null) {
+                temp1 = temp1.next;
+            }
+
+            ListNode temp2New = list2;
+
+            while(j < n) {
+                ListNode newNode = new ListNode(arr[j]);
+                temp1.next = newNode;
+                temp1 = temp1.next;
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
